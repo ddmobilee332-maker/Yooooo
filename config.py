da96a47a17da907a32fcb3f6a1e022c78a6e0c45
Johnny_niy😝 <ddmobilee332@gmail.com>
2026-06-01 14:17:38 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# การตั้งค่าสีระบบ (Red & Dark Theme)
+COLOR_RED = "\033[91m"
+COLOR_DARK = "\033[90m"
+COLOR_RESET = "\033[0m"
+COLOR_GREEN = "\033[92m"
+COLOR_WHITE = "\033[97m"
+
+SYSTEM_VERSION = "v2.0.4 (Ruenyai HUB Edition)"
